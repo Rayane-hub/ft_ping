7758bff5e10ee7a57506d7afe8fe4a23f2f73fd1
root <root@rasamad.myguest.virtualbox.org>
2026-01-20 20:19:20 +0100

    parsing in progress

--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
+int main(int ac, char **av)
 {
-	printf("Prog ft_ping\n\n");
+	if (ac <= 1)
+		return (printf("ping: missing host operand\nTry 'ping --help' or 'ping --usage' for more information.\n"), 64);
+	if(strcmp(av[1], "-?") == 0)
+		printf("print -?\n");
+
 }
+
+
+/*
+./ft_ping
+./ft_ping -?
+./ft_ping 8.8.8.8
+./ft_ping -v 8.8.8.8
+./ft_ping -x
+*/
