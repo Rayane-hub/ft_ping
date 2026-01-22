fbb8c5075220c625e1f92fd6291413061cb6226a
rasamad <rasamad@rasamad.myguest.virtualbox.org>
2026-01-22 17:52:17 +0100

    adress convert IPV4

--- src/main.c
+++ src/main.c
@@ -1,6 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <unistd.h>        // close
+#include <errno.h>         // errno
+#include <sys/types.h>    // types utilisés par le système (socklen_t, etc.)
+#include <sys/socket.h>   // socket()
+#include <netdb.h>        // getaddrinfo(), freeaddrinfo()
+#include <netinet/in.h>    // IPPROTO_ICMP
+
+#include <stdio.h>        // printf
+#include <string.h>       // memset, strcmp
+
+#include <arpa/inet.h>   // inet_ntop
+
+void ft_print_adrr(struct addrinfo *res)
+{
+	char ip_str[INET_ADDRSTRLEN];
+
+	struct sockaddr_in *addr_in = (struct sockaddr_in *)res->ai_addr;
+
+	inet_ntop(AF_INET, &addr_in->sin_addr, ip_str, sizeof(ip_str));
+
+	printf("Resolved IP = %s\n", ip_str);
+
+}
 
 int ft_flag(int ac, char **av, char **host)
 {
@@ -31,12 +51,43 @@ int main(int ac, char **av)
         char *host = NULL;
 	int check_v = ft_flag(ac, av, &host);
         printf("flag = %d\nhost = |%s|\n", check_v, host);
-        if (check_v == 0)
+        if (check_v != 0 && check_v != 2)
+                return (check_v);
+
+	/* =========================
+	   RESOLUTION HOST -> IP
+	   ========================= */
+
+	struct addrinfo hints;   // filtres: ce que l’on demande à l’OS
+	struct addrinfo *res;    // résultat: adresse(s) trouvée(s)
+
+	/* On met toute la structure à zéro pour éviter
+	   des valeurs aléatoires dans les champs */
+	memset(&hints, 0, sizeof(hints));
+
+	/* On demande uniquement de l’IPv4 (AF_INET) */
+	hints.ai_family = AF_INET;
+
+	/* Appel système: transforme "google.com" ou "8.8.8.8" en adresse réseau */
+	int ret = getaddrinfo(host, NULL, &hints, &res);
+	/* Si DNS / résolution échoue */
+	if (ret != 0)
+	    return(printf("ping: unknown host\n"), 64);
+	ft_print_adrr(res);
+
+	printf("Host resolved successfully\n");
+
+	/* getaddrinfo alloue de la mémoire → on libère */
+
+	if (check_v == 0)
                 printf("mode normal\n");
         else if (check_v == 2)
                 printf("mode verbose\n");
-        else
-                return (check_v);
+
+	/* Si on arrive ici: une adresse IPv4 a été trouvée */
+	freeaddrinfo(res);
+
+
         return (0);
 }
 
@@ -47,4 +98,11 @@ int main(int ac, char **av)
 ./ft_ping 8.8.8.8
 ./ft_ping -v 8.8.8.8
 ./ft_ping -x
+*/
+/*
+string host
+   ↓
+getaddrinfo()
+   ↓
+adresse réseau valide (struct sockaddr)
 */
