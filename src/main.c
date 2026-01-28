56030cf50b7450f70538b602a6478f01feb435cb
Rayane Samad <rayane.tisec77@gmail.com>
2026-01-28 21:06:11 +0100

    begin icmp build

--- src/main.c
+++ src/main.c
@@ -3,8 +3,10 @@
 #include <stdio.h>	// printf
 #include <string.h>	// memset, strcmp
 #include <arpa/inet.h> //inet_ntop()
-
+#include <stdlib.h>//exit()
+#include <netinet/ip_icmp.h>   // struct icmphdr
 #define INET_ADDRSTRLEN 16
+#define PACKET_SIZE 64
 
 
 int ft_flag(int ac, char **av, char **host) {
@@ -19,9 +21,11 @@ int ft_flag(int ac, char **av, char **host) {
 		{
 			if (!*host && av[i][1] == '\0')
 				return(printf("ping: unknown host\n"), 1); // '-' seul -> erreur
-			if (av[i][1] == '?')
-				return(printf("Usage: ft_ping [-v] destination\n-v    verbose output\n-?    display this help\n"), 0); // aide
-			else if (av[i][1] == 'v')
+			if (av[i][1] == '?'){
+				printf("Usage: ft_ping [-v] destination\n-v    verbose output\n-?    display this help\n"); // aide
+				exit(0);
+			}
+				else if (av[i][1] == 'v')
 				check_v = 2; // mode verbose
 			else if (av[i][1] != '\0')
 				return(printf("ping: invalid option -- '%c'\nTry 'ping --help' or 'ping --usage' for more information.\n", av[i][1]), 64); // option invalide
@@ -32,11 +36,25 @@ int ft_flag(int ac, char **av, char **host) {
 	return (check_v);
 }
 
-void ft_print_ip(struct sockaddr_in *addr, char *host)
-{
+void ft_print_ip(struct sockaddr_in *addr, char *host){
 	char buffer[INET_ADDRSTRLEN];
 	inet_ntop(AF_INET, &addr->sin_addr, buffer, INET_ADDRSTRLEN);
 	printf("\nIP de %s = %s\n\n", host, buffer);
+}
+
+int ft_icmp_builder()
+{
+	char packet[PACKET_SIZE];
+
+	struct icmphdr *icmp = (struct icmphdr *)packet;
+	icmp->type = 8;
+	icmp->code = 0;
+	icmp->checksum = 0;
+	//icmp->un.echo.id = ?
+	//icmp->un.echo.sequence = ?
+	
+	//calcul checksum
+	return 0;
 }
 
 int main(int ac, char **av) {
@@ -50,7 +68,7 @@ int main(int ac, char **av) {
 
 	struct addrinfo hints;
 	struct addrinfo *res;
-
+	
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_INET;
 
@@ -59,15 +77,17 @@ int main(int ac, char **av) {
 
 	struct sockaddr_in *addr = (struct sockaddr_in *)res->ai_addr;
 	ft_print_ip(addr, host);
-
+	
 	int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
 	if (sockfd == -1)
 		return(printf("err socket\n"), 1);
+
+	ft_icmp_builder();
 
 	if (check_v == 0)
                 printf("mode normal\n");
 	else if (check_v == 2)
 			printf("mode verbose\n");
 	freeaddrinfo(res);
-        return (0);
+    return (0);
 }
